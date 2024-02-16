327c1a1f7cab72f50c32edaee6d6c325350778fc
Guilherme Reis <guilhermedeveloperbr9@gmail.com>
2024-02-16 13:38:58 -0300

    Erro ao mostrar no display

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const botao = document.querySelector('input[type="button"][value="="]');
+const botoes = document.querySelectorAll("button");
+const displayResult = document.querySelector("#resultDisplay");
+let valorButtonPress;
 
-botao.addEventListener("click", function () {
-  console.log(botao.value);
-});
+function targetButton() {
+  for (const botao of botoes) {
+    botao.addEventListener("click", () => {
+      valorButtonPress = botao.textContent;
+
+      displayResult.innerHTML = valorButtonPress;
+    });
+  }
+}
+
+targetButton();
